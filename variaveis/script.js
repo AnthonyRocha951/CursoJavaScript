fe2c17b6b72801e2cdc52cf1d25fe9a4ffc71168
AnthonyRocha <rochaanthony159@gmail.com>
2024-07-10 18:34:04 -0300

    Initial commit

---
+++ variaveis/script.js
@@ -0,0 +1,56 @@
+var nome = 'Anthony'
+var idade = 18
+var possuiFaculdade = true
+
+console.log(nome, idade, possuiFaculdade)
+
+var preco = 25
+var totalComprado = 5
+var totalPreco = totalComprado * preco
+
+console.log(totalPreco)
+
+var sobrenome = 'Rocha',
+    cidade = 'São Paulo'
+
+console.log(nome, sobrenome, idade, cidade)
+
+var semDefinir
+console.log(semDefinir)
+
+// console.log(aindaNaoDefine)
+
+
+var comida
+console.log(comida)
+comida = 'Pizza'
+console.log(comida)
+
+// const time = 'Vasco'
+// time = 'Flamengo'
+
+// console.log(time)
+
+
+
+// EXERCICIO DA AULA ABAIXO
+
+// Declarar uma variável com o seu nome
+var nome = 'Anthony'
+
+// Declarar uma variável com a sua idade
+var idade = 18
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaFavorita
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = 'Parmegiana de Frango'
+
+// Declarar 5 variáveis diferentes sem valores
+var esporteFavorito,
+    filmeFavorito,
+    brinquedoFavorito,
+    sexo,
+    sonho
