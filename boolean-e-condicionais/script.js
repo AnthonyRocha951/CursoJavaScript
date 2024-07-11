fd4b75d2cbafdae6cf1f51c37a23a647795aaac1
AnthonyRocha <rochaanthony159@gmail.com>
2024-07-11 16:28:35 -0300

    Boolean e Condicionais

---
+++ boolean-e-condicionais/script.js
@@ -0,0 +1,33 @@
+var possuiGraduacao = false
+var possuiDoutorado = true
+
+if(possuiGraduacao) {
+    console.log('É verdadeiro')
+}
+
+else if(possuiDoutorado) {
+    console.log('Possui Doutorado')
+}
+
+else {
+    console.log('Não Possui nada')
+}
+
+var nome = '10kg' / 10
+
+if(nome) {
+    console.log(nome)
+}
+
+else {
+    console.log('Nome não existe')
+}
+
+if(!possuiGraduacao) {
+    console.log(possuiGraduacao)
+    console.log('Não possui graduaçao') // !! pra verificar se é true ou false
+}
+
+var x = 10
+
+console.log(x !== 14)
