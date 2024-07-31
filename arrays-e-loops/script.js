8a66408142d09e98414f61103bd54d8b3d098bc7
AnthonyRocha <rochaanthony159@gmail.com>
2024-07-31 15:13:32 -0300

    arrays-e-loops

---
+++ arrays-e-loops/script.js
@@ -0,0 +1,13 @@
+var videoGames = ['play2', 'play4', 'xbox360']
+
+var ultimoItem = videoGames.pop()
+
+for (var numero = 1; numero <= 10; numero++) {
+    console.log(numero)
+}
+
+var i = 0
+while (i < 10) {
+    console.log(i)
+    i++
+}
