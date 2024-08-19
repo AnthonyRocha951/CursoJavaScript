7ed4f5a4c708b52ba0384c37a54138b6f3172178
AnthonyRocha <rochaanthony159@gmail.com>
2024-08-19 13:57:13 -0300

    Js em Animais Fantásticos

--- Origamid/selecionar-o-dom/script.js
+++ Origamid/selecionar-o-dom/script.js
@@ -1,2 +1,22 @@
-const animais = document.getElementById('animais')
+const animais = document.getElementById('animais') // seleciona um id
 console.log(animais)
+
+const gridSection = document.getElementsByClassName('grid-section') //seleciona uma class
+console.log(gridSection[1])
+
+const ul = document.getElementsByTagName('ul') //selecionar uma tag
+console.log(ul[1])
+
+const primeiraLi = document.querySelector('li') // o querySelector sempre vai retornar o primeiro, ele nao retorna todos.
+console.log(primeiraLi)
+
+const primeiraUl = document.querySelector('ul')
+console.log(primeiraUl)
+
+const linkInterno = document.querySelector('[href^="#"]') // isso é pra selecionar algum link interno. ^ significa que começa com "#"
+console.log(linkInterno)
+
+// lembrando que voce tambem pode colocar metodos e propriedades como: .length
+
+const animaisImg = document.querySelectorAll('.animais img') // o querySelectorAll seleciona todos os elementos, por isso se chama ALL //estou pegando todas as img que estao na class .animais
+console.log(animaisImg[1])
